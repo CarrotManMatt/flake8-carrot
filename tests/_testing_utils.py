72cd27a98e3314a51e757c3d34d35f1e7b286fbd
Matt Norton <matt@carrotmanmatt.com>
2024-07-22 02:50:58 +0100

    Add `README.adoc` and tests

--- tests/_testing_utils.py
+++ tests/_testing_utils.py
@@ -13,5 +13,6 @@ from flake8_carrot.plugins.base import BasePlugin
 def apply_plugin_to_ast(raw_testing_ast: str, plugin_class: type[BasePlugin]) -> Set[str]:
     """"""
     return {
-        f"{line}:{column} {message}" for line, column, message, _ in plugin_class(ast.parse(raw_testing_ast)).run()
+        f"{line}:{column} {message}"
+        for line, column, message, _ in plugin_class(ast.parse(raw_testing_ast)).run()
     }
--- tests/test_carrot_plugin.py
+++ tests/test_carrot_plugin.py
@@ -2,7 +2,7 @@
 
 from collections.abc import Sequence
 
-__all__: Sequence[str] = ()
+__all__: Sequence[str] = ("TestTrivialCases",)
 
 from collections.abc import Set
 
@@ -13,3 +13,10 @@ from tests._testing_utils import apply_plugin_to_ast
 def _apply_carrot_plugin_to_ast(raw_testing_ast: str) -> Set[str]:
     """"""
     return apply_plugin_to_ast(raw_testing_ast, CarrotPlugin)
+
+
+class TestTrivialCases:
+    """"""
+
+    def test_empty_case(self) -> None:
+        assert _apply_carrot_plugin_to_ast("") == set()
