cb26dffd30c19b933c9f5ec9940e756d676340ce
Matt Norton <matt@carrotmanmatt.com>
2024-07-31 23:23:16 +0100

    Implement rule CAR006

---
+++ flake8_carrot/carrot/CAR006.py
@@ -0,0 +1,163 @@
+""""""  # noqa: N999
+from collections.abc import Sequence
+
+__all__: Sequence[str] = ("RuleCAR006",)
+
+import abc
+import ast
+from tokenize import TokenInfo
+from typing import Final, Literal, override
+
+from flake8_carrot.utils import BaseRule
+
+
+class RuleCAR006(BaseRule):
+    """"""
+
+    class _BaseVisitPassFlag(abc.ABC):
+        """"""
+
+        def __bool__(self) -> bool:
+            return self._get_true_value() is not None
+
+        @abc.abstractmethod
+        def _get_true_value(self) -> int | None:
+            """"""
+
+        @property
+        @abc.abstractmethod
+        def first_all_export_lineno(self) -> object:
+            """"""
+
+    class FirstVisitPassFlag(_BaseVisitPassFlag):
+        """"""
+
+        @override
+        def __init__(self, *, first_all_export_lineno: "RuleCAR006._BaseVisitPassFlag | int | None") -> None:  # noqa: E501
+            if isinstance(first_all_export_lineno, RuleCAR006._BaseVisitPassFlag):
+                first_all_export_lineno = first_all_export_lineno._get_true_value()  # noqa: SLF001
+
+            self._first_all_export_lineno: int | None = first_all_export_lineno
+
+        @property
+        @override
+        def first_all_export_lineno(self) -> int | None:
+            return self._first_all_export_lineno
+
+        @override
+        def _get_true_value(self) -> int | None:
+            return self.first_all_export_lineno
+
+    class SecondVisitPassFlag(_BaseVisitPassFlag):
+        """"""
+
+        @override
+        def __init__(self, *, first_all_export_lineno: "RuleCAR006._BaseVisitPassFlag | int") -> None:  # noqa: E501
+            if isinstance(first_all_export_lineno, RuleCAR006._BaseVisitPassFlag):
+                raw_first_all_export_lineno: int | None = (
+                    first_all_export_lineno._get_true_value()  # noqa: SLF001
+                )
+                if raw_first_all_export_lineno is None:
+                    raise ValueError
+
+                first_all_export_lineno = raw_first_all_export_lineno
+
+            self._first_all_export_lineno: int = first_all_export_lineno
+
+        @property
+        @override
+        def first_all_export_lineno(self) -> int:
+            return self._first_all_export_lineno
+
+        @override
+        def __bool__(self) -> Literal[True]:
+            if not super().__bool__():
+                raise RuntimeError
+
+            return True
+
+        @override
+        def _get_true_value(self) -> int | None:
+            return self.first_all_export_lineno
+
+    @override
+    def __init__(self) -> None:
+        self.visit_pass_flag: RuleCAR006._BaseVisitPassFlag = (
+            self.FirstVisitPassFlag(first_all_export_lineno=None)
+        )
+
+        super().__init__()
+
+    @classmethod
+    @override
+    def format_error_message(cls, ctx: dict[str, object]) -> str:
+        imported_class: object | None = ctx.get("imported_class", None)
+        if imported_class is not None and not isinstance(imported_class, str):
+            raise TypeError
+
+        return (
+            "CAR006 "
+            f"{
+                f"Importing `{imported_class}`, from `collections.abc`, is not"
+                if imported_class
+                else "Only `Sequence` import, from `collections.abc`, is"
+            } allowed above `__all__` export"
+        )
+
+    @override
+    def run_check(self, tree: ast.AST, file_tokens: Sequence[TokenInfo], lines: Sequence[str]) -> None:  # noqa: E501
+        if self.visit_pass_flag:
+            raise RuntimeError
+
+        self.visit(tree)
+
+        if not isinstance(self.visit_pass_flag, self.FirstVisitPassFlag):
+            raise RuntimeError  # noqa: TRY004
+
+        self.visit_pass_flag = self.SecondVisitPassFlag(
+            first_all_export_lineno=self.visit_pass_flag,
+        )
+
+        self.visit(tree)
+
+    @override
+    def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
+        COLLECTIONS_IMPORT_FOUND: Final[bool] = bool(
+            node.module in ("collections.abc", "typing")
+            and isinstance(self.visit_pass_flag, self.SecondVisitPassFlag)
+            and node.lineno < self.visit_pass_flag.first_all_export_lineno
+            and any(alias.name == "Sequence" for alias in node.names)  # noqa: COM812
+        )
+        if COLLECTIONS_IMPORT_FOUND:
+            alias: ast.alias
+            for alias in node.names:
+                if alias.name == "Sequence":
+                    continue
+
+                self.problems[(alias.lineno, alias.col_offset)] = {
+                    "imported_class": alias.name,
+                }
+
+        self.generic_visit(node)
+
+    @override
+    def visit_Assign(self, node: ast.Assign) -> None:
+        ALL_EXPORT_FOUND: Final[bool] = any(
+            isinstance(target, ast.Name) and target.id == "__all__"
+            for target in node.targets
+        )
+        if ALL_EXPORT_FOUND and not self.visit_pass_flag:
+            self.visit_pass_flag = self.FirstVisitPassFlag(first_all_export_lineno=node.lineno)
+
+        self.generic_visit(node)
+
+    @override
+    def visit_AnnAssign(self, node: ast.AnnAssign) -> None:
+        ALL_EXPORT_FOUND: Final[bool] = bool(
+            isinstance(node.target, ast.Name)
+            and node.target.id == "__all__"  # noqa: COM812
+        )
+        if ALL_EXPORT_FOUND and not self.visit_pass_flag:
+            self.visit_pass_flag = self.FirstVisitPassFlag(first_all_export_lineno=node.lineno)
+
+        self.generic_visit(node)
--- flake8_carrot/carrot/__init__.py
+++ flake8_carrot/carrot/__init__.py
@@ -17,6 +17,7 @@ from .CAR002 import RuleCAR002
 from .CAR003 import RuleCAR003
 from .CAR004 import RuleCAR004
 from .CAR005 import RuleCAR005
+from .CAR006 import RuleCAR006
 
 
 class CarrotPlugin(BasePlugin):
@@ -32,4 +33,6 @@ class CarrotPlugin(BasePlugin):
     @classproperty
     @override
     def RULES(cls) -> frozenset[type[BaseRule]]:  # noqa: N805
-        return frozenset({RuleCAR001, RuleCAR002, RuleCAR003, RuleCAR004, RuleCAR005})
+        return frozenset(
+            {RuleCAR001, RuleCAR002, RuleCAR003, RuleCAR004, RuleCAR005, RuleCAR006},
+        )
