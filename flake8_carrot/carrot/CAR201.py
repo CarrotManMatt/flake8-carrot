bc14ccecf04e3d23d0f7350d7d550eb0b4686e37
Matt Norton <matt@carrotmanmatt.com>
2024-08-03 14:44:30 +0100

    Improve selection range for rule CAR201

--- flake8_carrot/carrot/CAR201.py
+++ flake8_carrot/carrot/CAR201.py
@@ -7,8 +7,6 @@ __all__: Sequence[str] = ("RuleCAR201",)
 import ast
 from typing import Final, override
 
-import astpretty
-
 from flake8_carrot.utils import BaseRule
 
 
@@ -25,26 +23,29 @@ class RuleCAR201(BaseRule):
 
     @override
     def visit_Assign(self, node: ast.Assign) -> None:
+        targets: list[ast.Name] = [
+            target for target in node.targets if isinstance(target, ast.Name)
+        ]
+
         LOGGER_ASSIGNMENT_FOUND: Final[bool] = bool(
-            isinstance(node.value, ast.Call)
-            and (
-                bool(
-                    isinstance(node.value.func, ast.Attribute)
-                    and isinstance(node.value.func.value, ast.Name)
-                    and node.value.func.value.id == "logging"
-                    and node.value.func.attr == "getLogger"  # noqa: COM812
-                )
-                or bool(
-                    isinstance(node.value.func, ast.Name)
-                    and node.value.func.id == "getLogger"  # noqa: COM812
+            bool(
+                isinstance(node.value, ast.Call)
+                and (
+                    bool(
+                        isinstance(node.value.func, ast.Attribute)
+                        and isinstance(node.value.func.value, ast.Name)
+                        and node.value.func.value.id == "logging"
+                        and node.value.func.attr == "getLogger"  # noqa: COM812
+                    )
+                    or bool(
+                        isinstance(node.value.func, ast.Name)
+                        and node.value.func.id == "getLogger"  # noqa: COM812
+                    )
                 )  # noqa: COM812
-            )  # TODO: Also accept variable called `logger`
+            )
+            or len(targets) == 1 and "logger" in targets[0].id
         )
         if LOGGER_ASSIGNMENT_FOUND:
-            targets: list[ast.Name] = [
-                target for target in node.targets if isinstance(target, ast.Name)
-            ]
-
             self.problems.add_without_ctx(
                 (
                     node.lineno,
@@ -60,7 +61,6 @@ class RuleCAR201(BaseRule):
 
     @override
     def visit_AnnAssign(self, node: ast.AnnAssign) -> None:
-        astpretty.pprint(node)
         LOGGER_ASSIGNMENT_FOUND: Final[bool] = bool(
             bool(
                 bool(
@@ -90,11 +90,27 @@ class RuleCAR201(BaseRule):
                     and isinstance(node.annotation.slice, ast.Name)
                     and node.annotation.value.id == "Final"
                     and node.annotation.slice.id == "Logger"  # noqa: COM812
-                )  # noqa: COM812  # TODO: Also accept variable called `logger` or variable annotated as `Logger` or `logging.Logger`
+                )  # noqa: COM812
             )
-            # or bool(
-            #     isinstance(node.annotation, ast.Subscript)  # noqa: COM812
-            # )  # noqa: COM812
+            or bool(
+                isinstance(node.annotation, ast.Name)
+                and node.annotation.id == "Logger"  # noqa: COM812
+            )
+            or bool(
+                isinstance(node.annotation, ast.Attribute)
+                and isinstance(node.annotation.value, ast.Name)
+                and node.annotation.value.id == "logging"
+                and node.annotation.attr == "Logger"  # noqa: COM812
+            )
+            or bool(
+                isinstance(node.annotation, ast.Subscript)
+                and isinstance(node.annotation.value, ast.Name)
+                and isinstance(node.annotation.slice, ast.Attribute)
+                and isinstance(node.annotation.slice.value, ast.Name)
+                and node.annotation.value.id == "Final"
+                and node.annotation.slice.value.id == "logging"
+                and node.annotation.slice.attr == "Logger"  # noqa: COM812
+            )  # noqa: COM812
         )
         if LOGGER_ASSIGNMENT_FOUND:
             self.problems.add_without_ctx(
