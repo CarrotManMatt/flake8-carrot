35b3c782e3079d74448c264a80be464b6732f7a7
Matt Norton <matt@carrotmanmatt.com>
2024-09-01 01:09:10 +0100

    Reduce duplicated code in CAR120.py

--- flake8_carrot/carrot/CAR120.py
+++ flake8_carrot/carrot/CAR120.py
@@ -17,8 +17,8 @@ from flake8_carrot.utils import CarrotRule
 class RuleCAR120(CarrotRule):
     """"""
 
-    TYPE_IGNORE_REGEX: Final[str] = r"(\s*)#(\s*)type(\s*):(\s*)ignore(?:(\s*)\[(\s*)[a-z_-]+(\s*)((?:,\s*[a-z_-]+\s*)*(?:,\s*)?)])?"
-    NOQA_REGEX: Final[str] = r"(\s*)#(\s*)noqa(?:(\s*):(\s*)[A-Z0-9]+((?:\s*,\s*[A-Z0-9]+)*))?"
+    TYPE_IGNORE_REGEX: Final[str] = r"(\s*)#(\s*)type(\s*):(\s*)ignore(?:(\s*)\[(\s*)[a-z_-]+(\s*)((?:,\s*[a-z_-]+\s*)*)(?:,(\s*))?])?"
+    NOQA_REGEX: Final[str] = r"(\s*)#(\s*)noqa(?:(\s*):(\s*)[A-Z0-9]+((?:\s*,\s*[A-Z0-9]+)*)(?:\s*,)?)?"
 
     @classmethod
     @override
@@ -34,28 +34,31 @@ class RuleCAR120(CarrotRule):
         )
 
     @classmethod
-    def _get_single_type_ignore_error_locations(cls, match: re.Match[str]) -> dict[int, str]:
-        # noinspection DuplicatedCode
+    def _get_single_type_ignore_error_locations(cls, line: str, offset: int = 0) -> dict[int, str]:
+        match: re.Match[str] | None = re.search(f"{cls.TYPE_IGNORE_REGEX}\\Z", line)
+        if match is None:
+            return {}
+
         error_locations: dict[int, str] = {}
 
         if match.group(1) != "  ":
-            error_locations[match.span(1)[0]] = "Replace with 2 spaces"
+            error_locations[offset + match.span(1)[0]] = "Replace with 2 spaces"
 
         if match.group(2) != " ":
-            error_locations[match.span(2)[0]] = "Replace with a single space"
+            error_locations[offset + match.span(2)[0]] = "Replace with a single space"
 
         if match.group(3) != "":
-            error_locations[match.span(3)[0]] = "Remove all spaces"
+            error_locations[offset + match.span(3)[0]] = "Remove all spaces"
 
         if match.group(4) != " ":
-            error_locations[match.span(4)[0]] = "Replace with a single space"
+            error_locations[offset + match.span(4)[0]] = "Replace with a single space"
 
         group_number: int
         for group_number in (5, 6, 7):
             group: str | None = match.group(group_number)
             if group is None or group == "":
                 continue
-            error_locations[match.span(group_number)[0]] = "Remove all spaces"
+            error_locations[offset + match.span(group_number)[0]] = "Remove all spaces"
 
         group8: str | None = match.group(8)
         if group8 is not None:
@@ -64,34 +67,37 @@ class RuleCAR120(CarrotRule):
             group8_match: re.Match[str]
             for group8_match in re.finditer(r",(\s*)[a-z_-]+(\s*)", group8):
                 if group8_match.group(1) != " ":
-                    error_locations[GROUP8_MATCH_START + group8_match.span(1)[0]] = "Replace with a single space"
+                    error_locations[offset + GROUP8_MATCH_START + group8_match.span(1)[0]] = "Replace with a single space"
                 if group8_match.group(2) != "":
-                    error_locations[GROUP8_MATCH_START +  group8_match.span(2)[0]] = "Remove all spaces"
+                    error_locations[offset + GROUP8_MATCH_START +  group8_match.span(2)[0]] = "Remove all spaces"
 
-            trailing_comma_match: re.Match[str] | None = re.search(r",(\s*)\Z", group8)
-            if trailing_comma_match is not None and trailing_comma_match.group(1) != "":
-                error_locations[GROUP8_MATCH_START + trailing_comma_match.span(1)[0]] = "Remove all spaces"
+        trailing_comma_group: str | None = match.group(9)
+        if trailing_comma_group is not None and trailing_comma_group != "":
+            error_locations[offset + match.span(9)[0]] = "Remove all spaces"
 
         return error_locations
 
     @classmethod
-    def _get_single_noqa_error_locations(cls, match: re.Match[str]) -> dict[int, str]:
-        # noinspection DuplicatedCode
+    def _get_single_noqa_error_locations(cls, line: str, offset: int = 0) -> dict[int, str]:
+        match: re.Match[str] | None = re.search(f"{cls.NOQA_REGEX}\\Z", line)
+        if match is None:
+            return {}
+
         error_locations: dict[int, str] = {}
 
         if match.group(1) != "  ":
-            error_locations[match.span(1)[0]] = "Replace with 2 spaces"
+            error_locations[offset + match.span(1)[0]] = "Replace with 2 spaces"
 
         if match.group(2) != " ":
-            error_locations[match.span(2)[0]] = "Replace with a single space"
+            error_locations[offset + match.span(2)[0]] = "Replace with a single space"
 
         group3: str | None = match.group(3)
         if group3 is not None and group3 != "":
-            error_locations[match.span(3)[0]] = "Remove all spaces"
+            error_locations[offset + match.span(3)[0]] = "Remove all spaces"
 
         group4: str | None = match.group(4)
         if group4 is not None and group4 != " ":
-            error_locations[match.span(4)[0]] = "Replace with a single space"
+            error_locations[offset + match.span(4)[0]] = "Replace with a single space"
 
         group5: str | None = match.group(5)
         if group5 is not None:
@@ -100,101 +106,51 @@ class RuleCAR120(CarrotRule):
             group5_match: re.Match[str]
             for group5_match in re.finditer(r"(\s*),(\s*)[A-Z0-9]+", group5):
                 if group5_match.group(1) != "":
-                    error_locations[GROUP5_MATCH_START + group5_match.span(1)[0]] = "Remove all spaces"
+                    error_locations[offset + GROUP5_MATCH_START + group5_match.span(1)[0]] = "Remove all spaces"
                 if group5_match.group(2) != " ":
-                    error_locations[GROUP5_MATCH_START +  group5_match.span(2)[0]] = "Replace with a single space"
+                    error_locations[offset + GROUP5_MATCH_START +  group5_match.span(2)[0]] = "Replace with a single space"
 
         return error_locations
 
     @classmethod
     def _get_type_ignore_first_error_locations(cls, line: str) -> dict[int, str]:
-        error_locations: dict[int, str] = {}
-
         match: re.Match[str] | None = re.search(
-            f"{cls.TYPE_IGNORE_REGEX}{cls.NOQA_REGEX}\\Z",
+            f"(?P<type_ignore>{cls.TYPE_IGNORE_REGEX})(?P<noqa>{cls.NOQA_REGEX})\\Z",
             line,
         )
         if match is None:
-            return error_locations
+            return {}
 
-        error_locations = error_locations | cls._get_single_type_ignore_error_locations(match)
-
-        if match.group(9) != "  ":
-            error_locations[match.span(9)[0]] = "Replace with 2 spaces"
-
-        if match.group(10) != " ":
-            error_locations[match.span(10)[0]] = "Replace with a single space"
-
-        group11: str | None = match.group(11)
-        if group11 is not None and group11 != "":
-            error_locations[match.span(11)[0]] = "Remove all spaces"
-
-        group12: str | None = match.group(12)
-        if group12 is not None and group12 != " ":
-            error_locations[match.span(12)[0]] = "Replace with a single space"
-
-        group13: str | None = match.group(13)
-        if group13 is not None:
-            GROUP13_MATCH_START: Final[int] = match.span(13)[0]
-
-            group13_match: re.Match[str]
-            for group13_match in re.finditer(r"(\s*),(\s*)[A-Z0-9]+", group13):
-                if group13_match.group(1) != "":
-                    error_locations[GROUP13_MATCH_START + group13_match.span(1)[0]] = "Remove all spaces"
-                if group13_match.group(2) != " ":
-                    error_locations[GROUP13_MATCH_START +  group13_match.span(2)[0]] = "Replace with a single space"
-
-        return error_locations
+        return (
+            cls._get_single_type_ignore_error_locations(
+                match.group("type_ignore"),
+                offset=match.span("type_ignore")[0],
+            )
+            | cls._get_single_noqa_error_locations(
+                match.group("noqa"),
+                offset=match.span("noqa")[0],
+            )
+        )
 
     @classmethod
     def _get_noqa_first_error_locations(cls, line: str) -> dict[int, str]:
-        # noinspection DuplicatedCode
-        error_locations: dict[int, str] = {}
-
         match: re.Match[str] | None = re.search(
-            f"{cls.NOQA_REGEX}{cls.TYPE_IGNORE_REGEX}\\Z",
+            f"(?P<noqa>{cls.NOQA_REGEX})(?P<type_ignore>{cls.TYPE_IGNORE_REGEX})\\Z",
             line,
         )
         if match is None:
-            return error_locations
+            return {}
 
-        error_locations = error_locations | cls._get_single_noqa_error_locations(match)
-
-        if match.group(6) != "  ":
-            error_locations[match.span(6)[0]] = "Replace with 2 spaces"
-
-        if match.group(7) != " ":
-            error_locations[match.span(7)[0]] = "Replace with a single space"
-
-        if match.group(8) != "":
-            error_locations[match.span(8)[0]] = "Remove all spaces"
-
-        if match.group(9) != " ":
-            error_locations[match.span(9)[0]] = "Replace with a single space"
-
-        group_number: int
-        for group_number in (10, 11, 12):
-            group: str | None = match.group(group_number)
-            if group is None or group == "":
-                continue
-            error_locations[match.span(group_number)[0]] = "Remove all spaces"
-
-        group13: str | None = match.group(13)
-        if group13 is not None:
-            GROUP13_MATCH_START: Final[int] = match.span(13)[0]
-
-            group13_match: re.Match[str]
-            for group13_match in re.finditer(r",(\s*)[a-z_-]+(\s*)", group13):
-                if group13_match.group(1) != " ":
-                    error_locations[GROUP13_MATCH_START + group13_match.span(1)[0]] = "Replace with a single space"
-                if group13_match.group(2) != "":
-                    error_locations[GROUP13_MATCH_START +  group13_match.span(2)[0]] = "Remove all spaces"
-
-            trailing_comma_match: re.Match[str] | None = re.search(r",(\s*)\Z", group13)
-            if trailing_comma_match is not None and trailing_comma_match.group(1) != "":
-                error_locations[GROUP13_MATCH_START + trailing_comma_match.span(1)[0]] = "Remove all spaces"
-
-        return error_locations
+        return (
+            cls._get_single_noqa_error_locations(
+                match.group("noqa"),
+                offset=match.span("noqa")[0],
+            )
+            | cls._get_single_type_ignore_error_locations(
+                match.group("type_ignore"),
+                offset=match.span("type_ignore")[0],
+            )
+        )
 
     @classmethod
     def _get_all_error_locations(cls, line: str) -> Mapping[int, str]:
@@ -206,19 +162,13 @@ class RuleCAR120(CarrotRule):
         if noqa_first_error_locations:
             return noqa_first_error_locations
 
-        single_type_ignore_match: re.Match[str] | None = re.search(
-            f"{cls.TYPE_IGNORE_REGEX}\\Z",
-            line,
-        )
-        if single_type_ignore_match is not None:
-            return cls._get_single_type_ignore_error_locations(single_type_ignore_match)
+        single_type_ignore_error_locations: dict[int, str] = cls._get_single_type_ignore_error_locations(line)
+        if single_type_ignore_error_locations:
+            return single_type_ignore_error_locations
 
-        single_noqa_match: re.Match[str] | None = re.search(
-            f"{cls.NOQA_REGEX}\\Z",
-            line,
-        )
-        if single_noqa_match is not None:
-            return cls._get_single_noqa_error_locations(single_noqa_match)
+        single_noqa_error_locations: dict[int, str] = cls._get_single_noqa_error_locations(line)
+        if single_noqa_error_locations:
+            return single_noqa_error_locations
 
         return {}
 
